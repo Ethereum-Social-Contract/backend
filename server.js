84ae014c10b0d1a44b6a06437315bf7388c67806
Dist bit <distbit@pm.me>
2022-10-09 06:03:17 -0500

    new changes

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ import { Identity } from "@semaphore-protocol/identity"
 import { Group } from "@semaphore-protocol/group"
 import { generateProof } from "@semaphore-protocol/proof"
 import { verifyProof } from "@semaphore-protocol/proof"
+import { packToSolidityProof } from "@semaphore-protocol/proof"
+
+//the public key file of the consortium of arbitors who are responsible for de-anonymising transactions if reauested to, by a court
+//the secret data for each deposit into the pool is given, by the client, to this server 
 var consortiumPublicKey = fs.readFileSync('consortiumPubKey.pem', 'utf8')
 
 const app = express();
@@ -20,11 +24,26 @@ const app = express();
 app.use(express.json())
 app.use(bodyParser.json());
 
-async function generateDemoProof () {
-    const identity = new Identity()
+
+
+//This function allows for us to reconstrict the 
+async function reConstructGroup(){
     const group = new Group()
+    var MemberAddedEvents = [] // get from alchemy api 
+    for (newMember in MemberAddedEvents){
+        var identityCommitment = newMember[2];
+        group.addMembers(identityCommitment) //add the identity commitment specified in the event to the local group object
+    }
+    //Todo: this requires looping through all smart contract deposit events to reconstruct the identityCommitment merkle tree so that it can be manipulated in node js
+    return group
+}
+
+//generateMembershipProof() allows us to 
+async function generateMembershipProof(identitySecret, group) {
+    const identity = new Identity()
+    const group = reConstructGroup();
     const externalNullifier = BigInt(1)
-    const signal = "Hello world";
+    const signal = "withdrawFromPool";
     group.addMembers([identity.generateCommitment()])
 
     const fullProof = await generateProof(identity, group, externalNullifier, signal, {
@@ -35,8 +54,10 @@ async function generateDemoProof () {
 }
 
 
-async function verifyInclusionProof(inclusionProof, identityCommitment){
-    console.log(inclusionProof)
+async function generateIdentityCommitment(inclusionProof, identityCommitment){
+    console.log(inclusionProof);
+    
+    identity.generateCommitment();
     const verificationKey = JSON.parse(fs.readFileSync("./static/semaphore.json", "utf-8"))
     var validProof = await verifyProof(verificationKey, inclusionProof) 
     return validProof
